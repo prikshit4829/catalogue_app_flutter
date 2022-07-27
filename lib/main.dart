180206d00889f65925757a7553615128d2d579c2
prikshit4829 <prikshit4829@gmail.com>
2022-07-27 18:08:00 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:catalogue_app/home_page.dart';
+import 'package:catalogue_app/pages/home_page.dart';
+import 'package:catalogue_app/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.amber), //works on when ThemeMode.light
+      darkTheme:
+          ThemeData(brightness: Brightness.dark), //works on when ThemeMode.dark
+      initialRoute: "/home", //for testing purpose
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text(
+          'login page',
+          style: TextStyle(
+            fontSize: 40.0,
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+          ),
+          textScaleFactor: 2.0,
+        ),
+      ),
+    );
+  }
+}
